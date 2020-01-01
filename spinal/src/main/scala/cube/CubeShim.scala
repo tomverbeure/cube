c16f81d0a5e35a2895de79be2ca05bbf05cd280d
Tom Verbeure <hombre+github@gmail.com>
2019-12-31 22:18:54 -0800

    Working version with devboard rev 2.

--- spinal/src/main/scala/cube/CubeShim.scala
+++ spinal/src/main/scala/cube/CubeShim.scala
@@ -12,13 +12,24 @@ class CubeShim(internalOsc : Boolean = true) extends Component {
 
     val io = new Bundle {
         val clk25       = in(Bool)
-        val hwic_3      = out(Bool)
+//        val hwic_3      = out(Bool)
         val hwic_11     = out(Bool)
+        val hwic_12     = out(Bool)
         val hwic_13     = out(Bool)
-        val hwic_15     = out(Bool)
+        val hwic_14     = out(Bool)
+//        val hwic_15     = out(Bool)
+        val hwic_18     = out(Bool)
         val hwic_19     = out(Bool)
-        val hwic_21     = out(Bool)
-        val hwic_25     = out(Bool)
+        val hwic_20     = out(Bool)
+//        val hwic_21     = out(Bool)
+//        val hwic_25     = out(Bool)
+        val hwic_45     = out(Bool)
+//        val hwic_46     = out(Bool)
+        val hwic_47     = out(Bool)
+//        val hwic_48     = out(Bool)
+        val hwic_52     = out(Bool)
+        val hwic_53     = out(Bool)
+        val hwic_54     = out(Bool)
 
         val leds        = out(Bits(4 bits))
     }
@@ -28,13 +39,34 @@ class CubeShim(internalOsc : Boolean = true) extends Component {
     val cubeTop = new CubeTop(false)
     cubeTop.io.clk25        <> io.clk25
     cubeTop.io.leds         <> io.leds
-    cubeTop.io.hub75.clk    <> io.hwic_11
-    cubeTop.io.hub75.lat    <> io.hwic_13
+
+    /*
+    cubeTop.io.hub75.clk    <> io.hwic_45
+    cubeTop.io.hub75.lat    <> io.hwic_11
     cubeTop.io.hub75.oe_    <> io.hwic_3
-    cubeTop.io.hub75.row(0) <> io.hwic_19
-    cubeTop.io.hub75.row(1) <> io.hwic_21
-    cubeTop.io.hub75.row(2) <> io.hwic_15
-    cubeTop.io.hub75.r0     <> io.hwic_25
+    cubeTop.io.hub75.row(0) <> io.hwic_47
+    cubeTop.io.hub75.row(1) <> io.hwic_13
+    cubeTop.io.hub75.row(2) <> io.hwic_46
+    cubeTop.io.hub75.r0     <> io.hwic_20
+    cubeTop.io.hub75.g0     <> io.hwic_53
+    cubeTop.io.hub75.b0     <> io.hwic_19
+    cubeTop.io.hub75.r1     <> io.hwic_52
+    cubeTop.io.hub75.g1     <> io.hwic_18
+    cubeTop.io.hub75.b1     <> io.hwic_48
+    */
+
+    cubeTop.io.hub75.clk    <> io.hwic_11
+    cubeTop.io.hub75.lat    <> io.hwic_45
+    cubeTop.io.hub75.oe_    <> io.hwic_20
+    cubeTop.io.hub75.row(0) <> io.hwic_13
+    cubeTop.io.hub75.row(1) <> io.hwic_47
+    cubeTop.io.hub75.row(2) <> io.hwic_12
+    cubeTop.io.hub75.r0     <> io.hwic_54
+    cubeTop.io.hub75.g0     <> io.hwic_19
+    cubeTop.io.hub75.b0     <> io.hwic_53
+    cubeTop.io.hub75.r1     <> io.hwic_18
+    cubeTop.io.hub75.g1     <> io.hwic_52
+    cubeTop.io.hub75.b1     <> io.hwic_14
 }
 
 
--- spinal/src/main/scala/cube/Hub75Streamer.scala
+++ spinal/src/main/scala/cube/Hub75Streamer.scala
@@ -29,18 +29,31 @@ class Hub75Streamer(conf: Hub75Config) extends Component {
     val row_cntr = Counter(conf.panel_rows/2, bit_cntr.willOverflow)
 
     val col_offset = Counter(conf.panel_cols * 2, row_cntr.willOverflow)
-    val col_mul_row = (col_cntr.value.resize(log2Up(conf.panel_cols)) + (col_offset).resize(log2Up(conf.panel_cols))) * row_cntr.value
+    val col_mul_row_0 = (col_cntr.value.resize(log2Up(conf.panel_cols)) + (col_offset).resize(log2Up(conf.panel_cols))) * row_cntr.value
+    val col_mul_row_1 = (col_cntr.value.resize(log2Up(conf.panel_cols)) + (col_offset).resize(log2Up(conf.panel_cols))) * (row_cntr.value + 8)
 
-    val r = UInt(8 bits) 
-    r := ((col_mul_row)(col_mul_row.getWidth-1 downto col_mul_row.getWidth-8))
+    val r0 = UInt(8 bits) 
+    val r1 = UInt(8 bits) 
+    r0 := ((col_mul_row_0)(col_mul_row_0.getWidth-1 downto col_mul_row_0.getWidth-8))
+    r1 := ((col_mul_row_1)(col_mul_row_1.getWidth-1 downto col_mul_row_1.getWidth-8))
+
+    val g0 = UInt(8 bits)
+    val g1 = UInt(8 bits)
+    g0 := U(255, 8 bits) - (row_cntr.value << 4).resize(g0.getWidth)
+    g1 := U(255, 8 bits) - ((U(8, 4 bits) + (row_cntr.value)) << 4)
+
+    val b0 = UInt(8 bits)
+    val b1 = UInt(8 bits)
+    b0 := (col_offset<<1).resize(b0.getWidth)
+    b1 := (col_offset<<1).resize(b1.getWidth)
 
     output_fifo_wr.valid      := True
-    output_fifo_wr.payload(0) := (r >> (bit_cntr.value+(8-conf.bpc)))(0)
-    output_fifo_wr.payload(1) := False
-    output_fifo_wr.payload(2) := False
-    output_fifo_wr.payload(3) := (r >> (bit_cntr.value+(8-conf.bpc)))(0)
-    output_fifo_wr.payload(4) := False
-    output_fifo_wr.payload(5) := False
+    output_fifo_wr.payload(0) := (r0 >> (bit_cntr.value+(8-conf.bpc)))(0)
+    output_fifo_wr.payload(1) := (g0 >> (bit_cntr.value+(8-conf.bpc)))(0)
+    output_fifo_wr.payload(2) := (b0 >> (bit_cntr.value+(8-conf.bpc)))(0)
+    output_fifo_wr.payload(3) := (r1 >> (bit_cntr.value+(8-conf.bpc)))(0)
+    output_fifo_wr.payload(4) := (g1 >> (bit_cntr.value+(8-conf.bpc)))(0)
+    output_fifo_wr.payload(5) := (b1 >> (bit_cntr.value+(8-conf.bpc)))(0)
     output_fifo_wr.payload(6) := col_cntr === 0 && row_cntr === 0 && bit_cntr === 0
 
     val u_output_fifo = StreamFifo(
